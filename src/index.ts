5495d751a821b6d16fff8050ccff390b6c1f4646
Enijar <TheEnijar@gmail.com>
2023-07-22 22:36:24 +0100

    chore: scrap users and save to json file

---
+++ src/index.ts
@@ -0,0 +1,69 @@
+import * as fs from "node:fs/promises";
+import * as path from "node:path";
+import { parse, type HTMLElement } from "node-html-parser";
+import { unionBy, orderBy } from "lodash";
+import { SingleBar } from "cli-progress";
+
+const MAX_PAGE = 100;
+const PAGINATION_DELAY = 3000;
+const USERS_FILE_PATH = path.resolve(__dirname, "..", "data", "users.json");
+
+async function htmlFromUrl(url: string) {
+  const res = await fetch(url);
+  if (res.ok) {
+    return parse(await res.text());
+  }
+  return null;
+}
+
+function usersFromHtml(html: HTMLElement) {
+  const rows = html.querySelectorAll("table > tbody > tr");
+  return rows.map((element) => {
+    const col1 = element.querySelector("td:nth-child(1)");
+    const col2 = element.querySelector("td:nth-child(2) > a");
+    const col3 = element.querySelector("td:nth-child(3)");
+    return {
+      position: parseFloat(col1?.textContent ?? "0"),
+      username: col2?.textContent ?? "",
+      link: col2?.getAttribute("href") ?? "",
+      points: col3?.textContent,
+    };
+  });
+}
+
+async function saveUsers(users: ReturnType<typeof usersFromHtml>) {
+  const existingUsers = JSON.parse(await fs.readFile(USERS_FILE_PATH, "utf8"));
+  await fs.writeFile(USERS_FILE_PATH, JSON.stringify([...existingUsers, ...users], null, 2), "utf8");
+}
+
+const progress = new SingleBar({});
+
+progress.start(MAX_PAGE, 0);
+
+async function main(page = 1) {
+  if (page === 1) {
+    await fs.writeFile(USERS_FILE_PATH, JSON.stringify([]), "utf8");
+  }
+  if (page > MAX_PAGE) {
+    progress.stop();
+    console.log("Finished");
+    return;
+  }
+  const html = await htmlFromUrl(`https://www.hasbrorisk.com/en/leaderboard/2/1/rankPoints/${page}`);
+  if (html !== null) {
+    const users = usersFromHtml(html);
+    await saveUsers(users);
+  }
+  progress.update(page);
+  await new Promise((resolve) => setTimeout(resolve, PAGINATION_DELAY));
+  await main(page + 1);
+}
+
+main()
+  .then(() => {
+    process.exit(0);
+  })
+  .catch((err) => {
+    console.error(err);
+    process.exit(1);
+  });
